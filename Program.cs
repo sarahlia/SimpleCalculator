788c3db1b035d293208409e6b598bcf656d45f4a
sarahlia <sarah.l.nainggolan@gmail.com>
2020-09-15 17:06:40 -0500

    finish try-catch.

--- Program.cs
+++ Program.cs
@@ -10,15 +10,25 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-
-            string input = Console.ReadLine();
-            int convertedNumber;
-            bool isConvertedSuccessfully = int.TryParse(input, out convertedNumber);
-
-            if (!isConvertedSuccessfully)
+            try
             {
-                throw new Exception("Conversion was not successful.");
+                string input = Console.ReadLine();
+
+               try
+                {
+                    StringToIntConverter stringToIntConverter = new StringToIntConverter();
+                    stringToIntConverter.convert(input);
+                } catch (Exception ex) {
+                    Console.WriteLine("There was an error with conversion: {0}", ex.Message);
+                }
+
+                
+            } catch (Exception ex) {
+                Console.WriteLine("There was an error: {0}", ex.Message);
+            } finally {
+                Console.WriteLine("The rest of my application is still running.");
             }
+            
 
         }
     }
---
+++ StringToIntConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class StringToIntConverter
+    {
+        public int convert(string input)
+        {
+            try
+            {
+                int convertedNumber;
+                bool isConvertedSuccessfully = int.TryParse(input, out convertedNumber);
+
+                if (!isConvertedSuccessfully)
+                {
+                    throw new Exception("Conversion was not successful.");
+                }
+
+                return convertedNumber;
+            } catch (Exception ex) {
+                throw new Exception("new message", ex);
+            }
+
+        }
+    }
+}
