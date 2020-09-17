dc0207f14ef5823f7ac2d2d742723adaeaa160e6
sarahlia <sarah.l.nainggolan@gmail.com>
2020-09-16 22:23:26 -0500

    Finish unit testing for SimpleCalculator.

---
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            //1. setting up the data:
+            int number1 = 1;
+            int number2 = 2;
+            //2. processing the data through the method that you want to test:
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            //3. comparing the result against our assumption:
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
